3974cf24e762aa05367667f14ef541404f7f2393
sylviefiat <sylvie.fiat@ird.fr>
2018-02-09 16:02:58 +1100

    feat(home): home background image

--- src/client/app/components/home/home.component.ts
+++ src/client/app/components/home/home.component.ts
@@ -15,23 +15,10 @@ import { IAppState  } from '../../modules/ngrx/index';
   styleUrls: ['home.component.css']
 })
 export class HomeComponent implements OnInit {
-  newName: string;  
-  public names$: Observable<any>;
 
   constructor(private store: Store<IAppState>, public routerext: RouterExtensions) {}
 
   ngOnInit() {
   }
 
-
-  readAbout() {
-    // Try this in the {N} app
-    // {N} can use these animation options
-    this.routerext.navigate(['/about'], {
-      transition: {
-        duration: 1000,
-        name: 'slideTop',
-      }
-    });
-  }
 }
