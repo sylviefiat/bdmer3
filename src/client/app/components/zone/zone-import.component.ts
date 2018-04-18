7a9e4cc156764c8f37dc47eb8f3baa76e4034e33
Dubois Morgan <morgandubois@gmail.com>
2018-04-18 16:39:22 +1100

    feat (add xml -> parse to Geojson -> refactor the geojson)

--- src/client/app/components/zone/zone-import.component.ts
+++ src/client/app/components/zone/zone-import.component.ts
@@ -4,9 +4,13 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
+<<<<<<< 547936eba261b50369d974984b107e9d189ee5ad
 import { MapStaticService } from '../../modules/core/services/map-static.service';
 import * as togeojson from '@mapbox/togeojson';
 import * as area from '@mapbox/geojson-area';
+=======
+import * as togeojson from '@mapbox/togeojson';
+>>>>>>> feat (add xml -> parse to Geojson -> refactor the geojson)
 
 import { RouterExtensions, Config } from '../../modules/core/index';
 import { Platform, Zone } from '../../modules/datas/models/index';
@@ -49,39 +53,109 @@ export class ZoneImportComponent implements OnInit {
         });
     }
 
-    kmlToGeoJson(kml) {
-        const reader = new FileReader();
-        reader.readAsText(kml);
+    kmlToGeoJson(kml){
+      return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsText(kml);
 
-        const self = this;
+            const self = this;
 
-        reader.onload = function(event) {
-            const parser = new DOMParser();
-            const x = parser.parseFromString(reader.result, 'application/xml')
-            const geojson = togeojson.kml(x).features;
+            reader.onload = function(event) {
+              const parser = new DOMParser();
+              const x = parser.parseFromString(reader.result, 'application/xml')
+              const geojson = togeojson.kml(x).features;
 
-            for (let i in geojson) {
+              for(var i  in geojson){
+
                 delete geojson[i].properties['styleHash'];
                 delete geojson[i].properties['styleMapHash'];
                 delete geojson[i].properties['styleUrl'];
 
-                geojson[i].properties.code = self.platform.code + "_" + self.nameRefactorService.convertAccent(geojson[i].properties.name).split(' ').join('-').replace(/[^a-zA-Z0-9]/g, '');
+                geojson[i].properties.codezone = self.platform.code+"_"+self.convertName(geojson[i].properties.name).split(' ').join('-').replace(/[^a-zA-Z0-9]/g,'');
 
-                console.log(geojson[i].geometry)
-                const surface = area.geometry(geojson[i].geometry);
-
-                geojson[i].properties.surface = parseInt(surface.toString().split('.')['0']);
-
-                self.upload.emit(geojson[i]);
-            }
-        }
+              }
+              resolve();
+            };
+      });
+        
     }
+
 
     handleUpload(kmlFile: any): void {
         if (kmlFile.target.files && kmlFile.target.files.length > 0) {
             this.kmlToGeoJson(kmlFile.target.files['0']);
         }
     }
+
+    convertName(str){
+      var conversions = new Object();
+      conversions['ae'] = 'ä|æ|ǽ';
+      conversions['oe'] = 'ö|œ';
+      conversions['ue'] = 'ü';
+      conversions['Ae'] = 'Ä';
+      conversions['Ue'] = 'Ü';
+      conversions['Oe'] = 'Ö';
+      conversions['A'] = 'À|Á|Â|Ã|Ä|Å|Ǻ|Ā|Ă|Ą|Ǎ';
+      conversions['a'] = 'à|á|â|ã|å|ǻ|ā|ă|ą|ǎ|ª';
+      conversions['C'] = 'Ç|Ć|Ĉ|Ċ|Č';
+      conversions['c'] = 'ç|ć|ĉ|ċ|č';
+      conversions['D'] = 'Ð|Ď|Đ';
+      conversions['d'] = 'ð|ď|đ';
+      conversions['E'] = 'È|É|Ê|Ë|Ē|Ĕ|Ė|Ę|Ě';
+      conversions['e'] = 'è|é|ê|ë|ē|ĕ|ė|ę|ě';
+      conversions['G'] = 'Ĝ|Ğ|Ġ|Ģ';
+      conversions['g'] = 'ĝ|ğ|ġ|ģ';
+      conversions['H'] = 'Ĥ|Ħ';
+      conversions['h'] = 'ĥ|ħ';
+      conversions['I'] = 'Ì|Í|Î|Ï|Ĩ|Ī|Ĭ|Ǐ|Į|İ';
+      conversions['i'] = 'ì|í|î|ï|ĩ|ī|ĭ|ǐ|į|ı';
+      conversions['J'] = 'Ĵ';
+      conversions['j'] = 'ĵ';
+      conversions['K'] = 'Ķ';
+      conversions['k'] = 'ķ';
+      conversions['L'] = 'Ĺ|Ļ|Ľ|Ŀ|Ł';
+      conversions['l'] = 'ĺ|ļ|ľ|ŀ|ł';
+      conversions['N'] = 'Ñ|Ń|Ņ|Ň';
+      conversions['n'] = 'ñ|ń|ņ|ň|ŉ';
+      conversions['O'] = 'Ò|Ó|Ô|Õ|Ō|Ŏ|Ǒ|Ő|Ơ|Ø|Ǿ';
+      conversions['o'] = 'ò|ó|ô|õ|ō|ŏ|ǒ|ő|ơ|ø|ǿ|º';
+      conversions['R'] = 'Ŕ|Ŗ|Ř';
+      conversions['r'] = 'ŕ|ŗ|ř';
+      conversions['S'] = 'Ś|Ŝ|Ş|Š';
+      conversions['s'] = 'ś|ŝ|ş|š|ſ';
+      conversions['T'] = 'Ţ|Ť|Ŧ';
+      conversions['t'] = 'ţ|ť|ŧ';
+      conversions['U'] = 'Ù|Ú|Û|Ũ|Ū|Ŭ|Ů|Ű|Ų|Ư|Ǔ|Ǖ|Ǘ|Ǚ|Ǜ';
+      conversions['u'] = 'ù|ú|û|ũ|ū|ŭ|ů|ű|ų|ư|ǔ|ǖ|ǘ|ǚ|ǜ';
+      conversions['Y'] = 'Ý|Ÿ|Ŷ';
+      conversions['y'] = 'ý|ÿ|ŷ';
+      conversions['W'] = 'Ŵ';
+      conversions['w'] = 'ŵ';
+      conversions['Z'] = 'Ź|Ż|Ž';
+      conversions['z'] = 'ź|ż|ž';
+      conversions['AE'] = 'Æ|Ǽ';
+      conversions['ss'] = 'ß';
+      conversions['IJ'] = 'Ĳ';
+      conversions['ij'] = 'ĳ';
+      conversions['OE'] = 'Œ';
+      conversions['f'] = 'ƒ';
+
+      for(var i in conversions){
+          var re = new RegExp(conversions[i],"g");
+          str = str.replace(re,i);
+      }
+
+      return str;
+  }
+
+    // submit() {
+
+    //     if (this.zoneForm.valid) {
+    //         this.zoneForm.value.codePlatform=this.zoneForm.controls.codePlatform.value;
+    //         this.submitted.emit(this.zoneForm.value);
+    //     }
+    // }
+
 
     changeNeedHelp() {
         this.needHelp = !this.needHelp;
