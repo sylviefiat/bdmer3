1e1c93cdc9f9a61d0029fcd2b77bcefc0a1bf262
Dubois Morgan <morgandubois@gmail.com>
2018-04-23 16:15:27 +1100

    feat (display map home only if connected and online)

--- src/client/app/components/home/home.component.ts
+++ src/client/app/components/home/home.component.ts
@@ -1,12 +1,12 @@
 // libs
-import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, Input} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 
 // app
 import { RouterExtensions, Config } from '../../modules/core/index';
-import { IAppState  } from '../../modules/ngrx/index';
-
+import { IAppState, getisLoggedIn, getAuthState, getPlatformInApp } from '../../modules/ngrx/index';
+import { Platform } from '../../modules/datas/models/index';
 
 @Component({
   moduleId: module.id,
@@ -14,11 +14,27 @@ import { IAppState  } from '../../modules/ngrx/index';
   templateUrl: 'home.component.html',
   styleUrls: ['home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+
+  title: string = 'My first AGM project';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  loggedIn: boolean;
+  platforms$: Observable<Platform[]>;
+
+  public onlineOffline: boolean = navigator.onLine;
 
   constructor(private store: Store<IAppState>, public routerext: RouterExtensions) {}
 
   ngOnInit() {
-  }
+  	this.loggedIn = this.store["source"]["value"]["auth"]["loggedIn"];
+  	if(this.loggedIn){
+	  	console.log(this.store["source"]["value"]["auth"]["country"]["code"])
+	  	
+	  	this.platforms$ = this.store.let(getPlatformInApp);
 
+	  	console.log(this.store.let(getPlatformInApp));
+  	}
+  }
 }
