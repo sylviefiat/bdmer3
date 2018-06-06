b4e43d923814c7ebd7ebec0c8ba18f0c114e3522
Fiat <sylvie.fiat@ird.fr>
2018-06-06 11:27:53 +1100

    chore(gulp4): fixed gulp tasks

--- tools/tasks/seed/build.bundle.rxjs.ts
+++ tools/tasks/seed/build.bundle.rxjs.ts
@@ -18,12 +18,30 @@ export = (done: any) => {
     paths: {
       'n:*': 'node_modules/*',
       'rxjs/*': 'node_modules/rxjs/*.js',
+      'rxjs-compat/*': 'node_modules/rxjs-compat/*.js',
+      'rxjs/internal-compatibility/*': 'node_modules/rxjs/internal-compatibility/*.js',
+      'rxjs/testing/*': 'node_modules/rxjs/testing/*.js',
+      'rxjs/ajax/*': 'node_modules/rxjs/ajax/*.js',
+      'rxjs/operators/*': 'node_modules/rxjs/operators/*.js',
+      'rxjs/webSocket/*': 'node_modules/rxjs/webSocket/*.js',
     },
     map: {
       'rxjs': 'n:rxjs',
+      'rxjs-compat': 'n:rxjs-compat',
+      'rxjs/internal-compatibility': 'n:rxjs/internal-compatibility',
+      'rxjs/testing': 'n:rxjs/testing',
+      'rxjs/ajax': 'n:rxjs/ajax',
+      'rxjs/operators': 'n:rxjs/operators',
+      'rxjs/webSocket': 'n:rxjs/webSocket',
     },
     packages: {
       'rxjs': {main: 'Rx.js', defaultExtension: 'js'},
+      'rxjs-compat': {main: 'Rx.js', defaultExtension: 'js'},
+      'rxjs/internal-compatibility': {main: 'index.js', defaultExtension: 'js'},
+      'rxjs/testing': {main: 'index.js', defaultExtension: 'js'},
+      'rxjs/ajax': {main: 'index.js', defaultExtension: 'js'},
+      'rxjs/operators': {main: 'index.js', defaultExtension: 'js'},
+      'rxjs/webSocket': {main: 'index.js', defaultExtension: 'js'},
     }
   });
   builder.bundle('rxjs', 'node_modules/.tmp/Rx.min.js', options)
