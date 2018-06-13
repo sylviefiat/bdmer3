6daabc005adaeabff0707572501d0d30764ed206
Fiat <sylvie.fiat@ird.fr>
2018-06-13 15:13:11 +1100

    feat(home-map): layer switcher

--- src/client/app/components/home/map.component.ts
+++ src/client/app/components/home/map.component.ts
@@ -3,7 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable, Subscription, pipe, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
-import { LngLatBounds, Layer, LngLat, MapMouseEvent } from 'mapbox-gl';
+import { LngLatBounds, Layer, LngLat, MapMouseEvent, Map } from 'mapbox-gl';
 import * as Turf from '@turf/turf';
 
 import { RouterExtensions, Config } from '../../modules/core/index';
@@ -16,22 +16,30 @@ import { IAppState } from '../../modules/ngrx/index';
   selector: 'bc-home-map',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
+    <nav id="switcher">
+      <a href="#" (click)="changeView('countries')" [class.isOn]="isDisplayed('countries')">{{'COUNTRIES' | translate}}</a>
+      <a href="#" (click)="changeView('zones')" [class.isOn]="isDisplayed('zones')">{{'ZONES' | translate}}</a>
+      <a href="#" (click)="changeView('stations')" [class.isOn]="isDisplayed('stations')">{{'STATIONS' | translate}}</a>
+      <div class="subswitch">
+        <a href="#" (click)="changeBL()" [class.isOn]="!bl" title="{{'SATELLITE' | translate}}"><fa [name]="'globe'" [border]=false [size]=1></fa></a>
+        <a href="#" (click)="changeBL()" [class.isOn]="bl" title="{{'STREETS' | translate}}"><fa [name]="'map'" [border]=false [size]=1></fa></a>
+      </div>
+    </nav>
     <mgl-map
-    [style]="'mapbox://styles/mapbox/satellite-v9'"
+    [style]="bls[bl]"
     [fitBounds]="bounds$ | async"
     [fitBoundsOptions]="{
       padding: boundsPadding,
       maxZoom: zoomMaxMap
     }" 
-    (zoomEnd)="zoomChange($event)">
+    (zoomEnd)="zoomChange($event)"
+    (data)="styleChange($event)">
       <ng-container *ngIf="isDisplayed('countries')">
         <mgl-marker *ngFor="let marker of markersCountries"
           [lngLat]="marker.lngLat">
-          <div 
+          <div
             (click)="zoomOnCountry(marker.country)"
-            class="marker"
-            (mouseEnter)="cursorStyle = 'pointer'"
-            (mouseLeave)="cursorStyle = ''">
+            class="marker">
             <fa [name]="'map-marker'" [border]=false [size]=3></fa><br/>
             <span>{{marker.name}}</span>
           </div>
@@ -49,7 +57,9 @@ import { IAppState } from '../../modules/ngrx/index';
               'fill-color': '#AFEEEE',
               'fill-opacity': 0.3,
               'fill-outline-color': '#000'
-              }">            
+              }"
+            (mouseEnter)="cursorStyle = 'pointer'"
+            (mouseLeave)="cursorStyle = ''">            
           </mgl-layer>
           <mgl-layer
             id="zonestext"
@@ -107,14 +117,65 @@ import { IAppState } from '../../modules/ngrx/index';
   styles: [
     `    
     mgl-map {
-      height:         calc(100vh - 96px);
+      height: calc(100vh - 96px);
       width: 100%;
     }
     .mapboxgl-popup-content {
       color:black;
     }
+    .marker:hover {
+      cursor = 'pointer';
+    }
     @media screen and (max-width: 800px) {
       height:50vh;
+    }
+    #switcher {
+        background: #fff;
+        position: absolute;
+        z-index: 1;
+        top: 106px;
+        right: 10px;
+        border-radius: 3px;
+        width: 120px;
+        border: 1px solid rgba(0,0,0,0.4);
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    #switcher a {
+        font-size: 13px;
+        color: #404040;
+        display: block;
+        margin: 0;
+        padding: 0;
+        padding: 10px;
+        text-decoration: none;
+        border-bottom: 1px solid rgba(0,0,0,0.25);
+        text-align: center;
+    }
+
+    #switcher a:last-child {
+        border: none;
+    }
+
+    #switcher a:hover {
+        background-color: #f8f8f8;
+        color: #404040;
+    }
+
+    #switcher a.isOn {
+        background-color: #106cc8;
+        color: #ffffff;
+    }
+
+    #switcher a.isOn:hover {
+        background: #3074a4;
+    }
+    #switcher .subswitch {
+      display:flex;
+      flex-direction:row;
+    }
+    #switcher .subswitch a {
+      width:50%
     }
     `]
 })
@@ -140,28 +201,55 @@ export class MapComponent implements OnInit, OnChanges {
   stations: Station[] = [];
   layerStations$: Observable<object>;
 
-  show: string = 'countries';
+  show: string[] = ['countries'];
+  tmp: string[] = [];
+  bls: string[] = ['mapbox://styles/mapbox/satellite-v9','mapbox://styles/mapbox/streets-v9'];
+  bl: number;
 
   constructor() {
+    this.bl=0;
   }
 
   ngOnInit() {
     this.init();
+
   }
 
   ngOnChanges() {
     this.init();
   }
 
-  ngAfterViewInit() {
-  }
-
   zoomChange(event) {
     this.zoom = event.target.getZoom();
     
-    if(this.zoom<=this.zoomMinCountries) this.show='countries'; 
-    if(this.zoom<=this.zoomMaxStations && this.zoom>this.zoomMinCountries) this.show='countries,zones';   
-    if(this.zoom>this.zoomMaxStations) this.show='zones,stations';
+    if(this.zoom<=this.zoomMinCountries) this.show=[...this.show.filter(s => s!=='countries'),'countries']; 
+    if(this.zoom<=this.zoomMaxStations && this.zoom>this.zoomMinCountries) this.show=[...this.show.filter(s=> s!=='countries'&&s!=='zones'),'countries','zones'];   
+    if(this.zoom>this.zoomMaxStations) this.show=[...this.show.filter(s=>s!=='zones'&&s!=='stations'),'zones','stations'];
+  }
+
+  changeView(view: string) {
+    this.show=this.show.indexOf(view)>=0?[...this.show.filter(s => s!==view)]:[...this.show,view];
+  }
+
+  isDisplayed(layer: string){
+    return this.show.indexOf(layer)>=0;
+  }
+
+  changeBL() {
+    this.bl=this.bl?0:1;
+    // Remove all the layers to reload them on new style when baselayer is loaded
+    this.tmp = this.show;    
+    this.show = [];
+  }
+
+  styleChange(event){
+    // when style is loaded put back the layers
+    if(event.dataType==="style"){
+      if(this.tmp.length>0){ 
+        this.show=this.tmp;
+        this.tmp=[];
+      }
+    }
   }
 
   init() {
@@ -182,10 +270,6 @@ export class MapComponent implements OnInit, OnChanges {
       }
       
     }
-  }
-
-  isDisplayed(layer: string){
-    return this.show.indexOf(layer)>=0;
   }
 
   zoomToCountries(coordinates): LngLatBounds {   
@@ -240,13 +324,7 @@ export class MapComponent implements OnInit, OnChanges {
     return bounds;
   }
 
-  showPopupZone(evt: MapMouseEvent) {
-    console.log((<any>evt).features[0].geometry.coordinates[0][0]);
-    this.selectedZone = (<any>evt).features[0];
-  }
-
   showPopupStation(evt: MapMouseEvent) {
-    console.log((<any>evt).features[0].geometry.coordinates);
     this.selectedStation = (<any>evt).features[0];
   }
 }
