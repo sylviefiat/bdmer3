19c424b1960764341d1824e6639b9b2fe737c0e0
Fiat <sylvie.fiat@ird.fr>
2018-07-12 17:56:09 +1100

    fix(country-list): missing effect init after opendb change

--- src/client/app/modules/countries/effects/countries.effect.ts
+++ src/client/app/modules/countries/effects/countries.effect.ts
@@ -16,72 +16,80 @@ import { IAppState, getServiceUrl } from '../../ngrx/index';
 
 @Injectable()
 export class CountriesEffects {
-  private basePath: string;
-  private dbname;
-  /**
-   * This effect does not yield any actions back to the store. Set
-   * `dispatch` to false to hint to @ngrx/effects that it should
-   * ignore any elements of this effect stream.
-   *
-   * The `defer` observable accepts an observable factory function
-   * that is called when the observable is subscribed to.
-   * Wrapping the database open call in `defer` makes
-   * effect easier to test.
-   */
-  @Effect({dispatch:false})
-  openDB$: Observable<any> = this.actions$    
-    .ofType<AppInitAction.FinishAppInitAction>(AppInitAction.ActionTypes.FINISH_APP_INIT)
-    .pipe(
-      withLatestFrom(this.store.select(getServiceUrl)),
-      map((value) => this.countriesService.initDB('countries', value[1]))
-    );
+    private basePath: string;
+    private dbname;
+    /**
+     * This effect does not yield any actions back to the store. Set
+     * `dispatch` to false to hint to @ngrx/effects that it should
+     * ignore any elements of this effect stream.
+     *
+     * The `defer` observable accepts an observable factory function
+     * that is called when the observable is subscribed to.
+     * Wrapping the database open call in `defer` makes
+     * effect easier to test.
+     */
+    @Effect({ dispatch: false })
+    openDB$: Observable<any> = this.actions$
+        .ofType<AppInitAction.FinishAppInitAction>(AppInitAction.ActionTypes.FINISH_APP_INIT)
+        .pipe(
+            withLatestFrom(this.store.select(getServiceUrl)),
+            map((value) => this.countriesService.initDB('countries', value[1]))
+        );
 
-  @Effect()
-  loadCountries$: Observable<Action> = this.actions$
-    .ofType<CountriesAction.LoadAction>(CountriesAction.ActionTypes.LOAD)
-    .pipe(
-      switchMap(load => this.countriesService.getAll()),
-      map((countries: Country[]) => new CountriesAction.LoadSuccessAction(countries)),
-      catchError(error => of(new CountriesAction.LoadFailAction(error)))
-    );
+    @Effect() init$: Observable<Action> = this.actions$
+        .ofType<CountriesAction.InitAction>(CountriesAction.ActionTypes.INIT)
+        .pipe(
+            switchMap(init => this.countryListService.getCountryList()),
+            map(countryList => new CountriesAction.InitializedAction(countryList)),
+            catchError((error) => of(new CountriesAction.InitFailedAction()))
+        );
 
-  @Effect()
-  addCountryToCountries$: Observable<Action> = this.actions$
-    .ofType<CountriesAction.AddCountryAction>(CountriesAction.ActionTypes.ADD_COUNTRY)
-    .pipe(
-      map((action: CountriesAction.AddCountryAction) => action.payload),
-      mergeMap(country => this.countriesService.addCountry(country)),
-      map((country) => new CountriesAction.AddCountrySuccessAction(country)),
-      catchError((country) => of(new CountriesAction.AddCountryFailAction(country)))
-    );
+    @Effect()
+    loadCountries$: Observable<Action> = this.actions$
+        .ofType<CountriesAction.LoadAction>(CountriesAction.ActionTypes.LOAD)
+        .pipe(
+            switchMap(load => this.countriesService.getAll()),
+            map((countries: Country[]) => new CountriesAction.LoadSuccessAction(countries)),
+            catchError(error => of(new CountriesAction.LoadFailAction(error)))
+        );
 
-  @Effect({ dispatch: false }) addCountrySuccess$ = this.actions$
-    .ofType<CountriesAction.AddCountrySuccessAction>(CountriesAction.ActionTypes.ADD_COUNTRY_SUCCESS)
-    .pipe(tap(() => this.router.navigate(['/countries']))
-    );
-  @Effect({ dispatch: false }) removeCountrySuccess$ = this.actions$
-    .ofType<CountriesAction.AddCountryFailAction>(CountriesAction.ActionTypes.REMOVE_COUNTRY_SUCCESS)
-    .pipe(tap(() => this.router.navigate(['/countries']))
-    );
-  @Effect()
-  removeCountryFromCountries$: Observable<Action> = this.actions$
-    .ofType<CountriesAction.RemoveCountryAction>(CountriesAction.ActionTypes.REMOVE_COUNTRY)
-    .pipe(
-      map((action: CountriesAction.RemoveCountryAction) => action.payload),
-      mergeMap(country => this.countriesService.removeCountry(country)),
-      map((country) => new CountriesAction.RemoveCountrySuccessAction(country)),
-      catchError((country) => of(new CountriesAction.RemoveCountryFailAction(country)))
-    );
+    @Effect()
+    addCountryToCountries$: Observable<Action> = this.actions$
+        .ofType<CountriesAction.AddCountryAction>(CountriesAction.ActionTypes.ADD_COUNTRY)
+        .pipe(
+            map((action: CountriesAction.AddCountryAction) => action.payload),
+            mergeMap(country => this.countriesService.addCountry(country)),
+            map((country) => new CountriesAction.AddCountrySuccessAction(country)),
+            catchError((country) => of(new CountriesAction.AddCountryFailAction(country)))
+        );
 
-  constructor(
-    private actions$: Actions,
-    private router: Router,
-    private store: Store<IAppState>,
-    private countriesService: CountriesService,
-    private countryListService: CountryListService,
-    private environment: AppService) {
-    //this.basePath = environment.config.servicesBasePath;
-    //this.dbname = "countries";
+    @Effect({ dispatch: false }) addCountrySuccess$ = this.actions$
+        .ofType<CountriesAction.AddCountrySuccessAction>(CountriesAction.ActionTypes.ADD_COUNTRY_SUCCESS)
+        .pipe(tap(() => this.router.navigate(['/countries']))
+        );
+    @Effect({ dispatch: false }) removeCountrySuccess$ = this.actions$
+        .ofType<CountriesAction.AddCountryFailAction>(CountriesAction.ActionTypes.REMOVE_COUNTRY_SUCCESS)
+        .pipe(tap(() => this.router.navigate(['/countries']))
+        );
+    @Effect()
+    removeCountryFromCountries$: Observable<Action> = this.actions$
+        .ofType<CountriesAction.RemoveCountryAction>(CountriesAction.ActionTypes.REMOVE_COUNTRY)
+        .pipe(
+            map((action: CountriesAction.RemoveCountryAction) => action.payload),
+            mergeMap(country => this.countriesService.removeCountry(country)),
+            map((country) => new CountriesAction.RemoveCountrySuccessAction(country)),
+            catchError((country) => of(new CountriesAction.RemoveCountryFailAction(country)))
+        );
 
-  }
+    constructor(
+        private actions$: Actions,
+        private router: Router,
+        private store: Store<IAppState>,
+        private countriesService: CountriesService,
+        private countryListService: CountryListService,
+        private environment: AppService) {
+        //this.basePath = environment.config.servicesBasePath;
+        //this.dbname = "countries";
+
+    }
 }
